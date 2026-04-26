a7feca3587ecaef6524dafe0d09fcb98010f2cc7
Rasmus Pernesten <Raddarin@localhost.localdomain>
2026-04-26 18:44:26 +0200

    massa buggar borta men funkar ej

--- labb/labb2/MyHash.hpp
+++ labb/labb2/MyHash.hpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <stddef.h>
 #include <vector>
 template <typename K, typename V>
@@ -12,17 +13,13 @@ public:
   MyHash() : size(1), elements(0) { bucket_list.resize(size); }
 
   V get(K key_in) {
-    if (elements == 0) {
-      return V();
+    int index = keyhash(key_in) % size;
+    if (bucket_list[index].size() == 1) {
+      return bucket_list[index][0].val;
     } else {
-      int index = keyhash(key_in) % size;
-      if (bucket_list[index].size == 1) {
-        return bucket_list[index][0];
-      } else {
-        for (KeyValPair &node : bucket_list[index]) {
-          if (node.key == key_in) {
-            return node.val;
-          }
+      for (KeyValPair &node : bucket_list[index]) {
+        if (node.key == key_in) {
+          return node.val;
         }
       }
     }
@@ -33,14 +30,15 @@ public:
     int index = keyhash(new_key) % size;
     for (KeyValPair &node : bucket_list[index]) {
       if (node.key == new_key) {
-        node.key = new_key;
+        node.val = new_val;
+        return;
       }
     }
     bucket_list[index].push_back({new_key, new_val});
     elements++;
     if ((float)elements / size > 0.5) {
       size = 2 * size;
-      bucket_list.resize(size);
+      reyhash();
     }
   }
 
@@ -48,13 +46,13 @@ public:
     int index = keyhash(key_in) % size;
     std::vector<KeyValPair> &chain = bucket_list[index];
 
-    for (KeyValPair &node = chain.begin(); node != chain.end(); ++node) {
-      if (node.key == key_in) {
+    for (auto node = chain.begin(); node != chain.end(); ++node) {
+      if (node->key == key_in) {
         chain.erase(node);
         elements--;
         if ((float)elements / size < 0.25) {
           size = size / 2;
-          bucket_list.resize(size);
+          reyhash();
         }
         return true;
       }
@@ -74,10 +72,20 @@ public:
   MyHash(const MyHash &) = default;
   MyHash &operator=(MyHash &&) = default;
   MyHash &operator=(const MyHash &) = default;
-  ~MyHash();
+  ~MyHash() = default;
 
 private:
   int size;
   int elements;
   std::vector<std::vector<KeyValPair>> bucket_list;
+  size_t keyhash(const K &key) const { return std::hash<K>{}(key); }
+  void reyhash() {
+    std::vector<KeyValPair> temp_list = getAll();
+    bucket_list.clear();
+    elements = 0;
+    bucket_list.resize(size);
+    for (KeyValPair node : temp_list) {
+      put(node.key, node.val);
+    }
+  }
 };
